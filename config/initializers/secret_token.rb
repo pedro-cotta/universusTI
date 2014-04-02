894f47f27ddd8bf2518e3e9edebe68b0f3a4534c
paulo <paulo@paulo-W240BU.(none)>
2014-04-02 01:19:40 -0300

    commit initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UniversusTI::Application.config.secret_key_base = 'c12c2374826f06d2cc333f2aa89b4d04ff12bc047e9329674416e9736342511b7a1e52243e8a9f6b0592994a381512dc556edb66a7a89148dd1f6aea9fa28280'
